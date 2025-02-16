da0e174da3cb70157d4cd5b3a7986c90b835b815
DG <daxtondelecluse@gmail.com>
2025-02-16 00:05:41 -0500

    Update MenuMusic.cs

--- objects/scenes/main_menu/MenuMusic.cs
+++ objects/scenes/main_menu/MenuMusic.cs
@@ -4,13 +4,19 @@ using System;
 public partial class MenuMusic : AudioStreamPlayer
 {
 
-	private byte menuStage = 1;
+	private byte menuStage = 0;
 	private AudioStreamSynchronized streamSync;
+
+	private Tween tween;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		//:3
+		// what the fuck
+
+		streamSync = (AudioStreamSynchronized)Stream;
+		tween = CreateTween();
 		GD.Print(Stream.GetType());
 	}
 
@@ -18,15 +24,16 @@ public partial class MenuMusic : AudioStreamPlayer
 	public override void _Process(double delta)
 	{
 		
-		streamSync = (AudioStreamSynchronized)Stream;
+	
 		if(Input.IsActionJustPressed("select"))
-			if(menuStage < 4)
+			if(menuStage < 3)
 			{
-				streamSync.SetSyncStreamVolume(menuStage, 0);
+				tween.TweenProperty(GetParent(), "stream_" + menuStage + "/volume", 0, 1.0f);
+				//streamSync.SetSyncStreamVolume(menuStage, 0);
 				menuStage++;
 			}
 		if(Input.IsActionJustPressed("back"))
-			if(menuStage > 1)
+			if(menuStage > 0)
 			{
 				streamSync.SetSyncStreamVolume(menuStage, -80);
 				menuStage--;
