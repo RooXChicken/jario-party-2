512d830effcef68649ae146d2f9a5d1f7d24df53
RooXChicken <rooxchicken@gmail.com>
2025-07-04 19:25:46 -0400

    Scale change

--- scenes/minigames/jump_rope/jump_rope.gd
+++ scenes/minigames/jump_rope/jump_rope.gd
@@ -1,10 +1,11 @@
 extends Minigame
 
 @export var flame_curve: Curve;
+@export var size_curve: Curve;
 
 const fire_red := 2.8;
 
-var heights := [6, 18, 32, 44, 48, 49, 49, 49, 48, 44, 32, 18, 6];
+var heights := [0, 18, 32, 44, 48, 49, 49, 49, 48, 44, 32, 18, 0];
 var time := 0.0;
 
 @export var speed_mult := 0.03;
@@ -19,10 +20,13 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	var sample_time: float = time - floor(time);
 	var sample := (flame_curve.sample(sample_time) - 0.5) * 2;
+	var size := size_curve.sample(sample_time);
 	
 	for i in range(0, 13):
 		var fire: Node2D = get_node("Flames/Flame" + str(i+1));
 		fire.position.y = sample * heights[i];
+		fire.scale = Vector2(size, size);
+		
 		fire.z_index = 2 if sample_time < 0.5 else 1;
 		
 		if(sample_time > 0.42 && sample_time < 0.58):
